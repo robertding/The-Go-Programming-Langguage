04badd1cde169783ac5108f15bedc489f5c4abd6
robert <robertdingx@gmail.com>
2018-09-27 00:43:04 +0800

    5-4

---
+++ ch5/5-3/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fmt"
+	"golang.org/x/net/html"
+	"net/http"
+	"os"
+)
+
+func main() {
+	url := "https://m.zhihu.com"
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "get url fail: %v\n", err)
+		os.Exit(1)
+	}
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+func visit(links []string, n *html.Node) []string {
+	if n == nil {
+		return links
+	}
+	if n.Type == html.ElementNode {
+		for _, a := range n.Attr {
+			switch a.Key {
+			case "href", "src", "img":
+				links = append(links, a.Val)
+			}
+		}
+	}
+	links = visit(links, n.NextSibling)
+	return visit(links, n.FirstChild)
+}
